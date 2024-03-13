76dd18ed95bd368920be639f45539da974c57118
wahyukurniawan556 <pujiwahyu556@gmail.com>
2024-03-13 23:37:12 +0700

    materi 11

---
+++ dart_project/bin/materi/11_operator.dart
@@ -0,0 +1,15 @@
+void main() {
+    // aritmatika
+    int c = 6;
+    c = c + 6;
+
+    // relasional
+    String kota = 'Jepara';
+    if (kota == 'Jepara') ;
+
+    // Logika
+    var A = true;
+    var B = false;
+    var expr = A && B; //false
+    print(expr);
+}
